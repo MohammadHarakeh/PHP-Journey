83e3fc7911ee9b0caac37158df8428dff7392a84
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-03-11 20:33:12 +0200

    filter numbers and sort done

--- filter.php
+++ filter.php
@@ -1,3 +1,24 @@
 <?php
-function filterNumber()
+function filterNumber($str){
+    $splitStr = str_split($str);
+    $numberArray = [];
+    $currentNumber = '';
+
+    for($i = 0; $i < strlen($str); $i++){
+        if(is_numeric($splitStr[$i])){
+            $currentNumber .= $splitStr[$i];
+        } else{
+            if($currentNumber !== ''){
+                $numberArray[] = (int)$currentNumber;
+                $currentNumber = '';
+            }
+        }
+    }
+    if($currentNumber !== ''){
+        $numberArray[] = (int)$currentNumber;
+    }
+    sort($numberArray);
+    echo "[" .implode(', ', $numberArray) ."]";
+}
+filterNumber("hpd12aq3@8w$5");
 ?>
